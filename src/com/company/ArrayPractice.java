d0f39a6f2ab66c06b6ca53fa718b2db67918d48a
AutoTau <zenbenjam@gmail.com>
2021-10-10 15:42:27 -0700

    Added an implementation for finding the largest hourglass sum in a 2d array

--- src/com/company/ArrayPractice.java
+++ src/com/company/ArrayPractice.java
@@ -1,16 +1,7 @@
 package com.company;
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.sql.Array;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
+
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 
 public class ArrayPractice {
@@ -53,6 +44,57 @@ public class ArrayPractice {
         return reversedArr;
     }
 
+    //An hourglass in A is a subset of values with indices falling in this pattern in A's graphical representation:
+    //
+    //  a b c
+    //    d
+    //  e f g
+    //
+    // This function returns the maximum hourglass sum in the given 2d array
+    public static int hourGlassSum(Integer[][] arr) {
+
+        /*
+            arr[0][0] is the top left cell of an hour glass
+            I need to sum the following:
+            top row:
+                arr[i][j]
+                arr[i][j+1]
+                arr[i][j+2]
+            middle value:
+                arr[i+1][j+1]
+            bottom row:
+                arr[i+2][j]
+                arr[i+2][j+1]
+                arr[i+2][j+2]
+        */
+
+        if (arr.length <= 1)
+            return 0;
+
+        int largestHourGlassSum = 0;
+        int hourGlassSum = 0;
+        for(int i = 0; i < arr.length - 2; i++) {
+            for (int j = 0; j < arr.length - 2; j++) {
+                hourGlassSum += arr[i][j];
+                hourGlassSum += arr[i][j + 1];
+                hourGlassSum += arr[i][j + 2];
+                hourGlassSum += arr[i + 1][j + 1];
+                hourGlassSum += arr[i + 2][j];
+                hourGlassSum += arr[i + 2][j + 1];
+                hourGlassSum += arr[i + 2][j + 2];
+
+                if (hourGlassSum > largestHourGlassSum)
+                    largestHourGlassSum = hourGlassSum;
+
+                hourGlassSum = 0;
+            }
+        }
+
+        return largestHourGlassSum;
+    }
+
+
+
 
     public static void main(String[] args) {
 
@@ -94,6 +136,32 @@ public class ArrayPractice {
         for (int i : reversedList) {
             System.out.print(i + " ");
         }
+
+        ////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+
+        System.out.println("");
+        System.out.println("");
+        //Finding the hourglass sum of a 2d array
+        Integer[][] hourglass = new Integer[6][6];
+        Random rand = new Random();
+
+        for (int j = 0; j < 6; j++) {
+            for (int i = 0; i < 6; i++) {
+                hourglass[i][j] = rand.nextInt(10);
+            }
+        }
+
+        System.out.println("Original 2d Array: ");
+        for (int j = 0; j < 6; j++) {
+            for (int i = 0; i < 6; i++) {
+                System.out.print(hourglass[i][j] + " ");
+            }
+        }
+
+        int largest_hour_glass_sum = hourGlassSum(hourglass);
+        System.out.println("");
+        System.out.println("Largest hour glass sum is: " + largest_hour_glass_sum);
 
     }
 }
